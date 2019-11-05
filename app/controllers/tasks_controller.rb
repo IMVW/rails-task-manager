113f16b83cd97f06c16c5ef69845b9d0833d1e73
Iris Vermeulen <iris.m.vermeulen@gmail.com>
2019-11-05 15:48:22 +0100

    inbetween save

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,27 @@
+class TasksController < ApplicationController
+  # __________READ____________
+
+  # Read (all)
+  def index
+    @tasks = Task.all
+  end
+
+  # Read (one)
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  # __________CREATE____________
+
+  # To show the form
+  def new
+    @task = Task.new
+  end
+
+  # create a new task
+  def create
+    @taks = Task.new(task_params)
+    @task.save
+    redirect_to task_path(@task)
+  end
+end
